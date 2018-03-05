b1f4e4c9b037d2632f064fa925819b58b0505100
denys.design@gmail.com <denys.design@gmail.com>
2018-03-05 18:49:03 -0300

    init configs

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Express is working on IISNode!');
+});
+
+app.get('/rotaTeste', function (req, res) {
+    res.send('Express is cronograma on IISNode!');
+});
+
+
+app.listen(process.env.PORT);
