a187ab304f50c2a36dcb6a4ab691e8200bf038a3
Denys Fontenelle Araujo <stef.denys.utic@sebrae.com.br>
2018-03-12 13:11:34 -0300

    novo server commit

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 var express = require('express');
 const path = require('path');
 var app = express();
-var cors = require('cors');
-app.use(cors());
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-var rp = require('request-promise');
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// router
-app.use('/', require('./routes'));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
-// Start nas rotinas
-var Rotinas = require('./controllers/Rotinas').start();
 
 app.listen(process.env.PORT || 3001, () => console.log('Running Angular'));
